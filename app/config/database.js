9350653572d71979704788d4d569b3caab6dd682
Keshav Katwe <keshav.katwe@gmail.com>
2015-03-15 23:16:19 +0530

    database update

--- app/config/database.js
+++ app/config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-        url : 'mongodb://localhost:27017/freelance'
+        url : 'mongodb://keshav.katwe:9986497408@ds045998.mongolab.com:45998/freelance'
     };
--- server.js
+++ server.js
@@ -11,7 +11,7 @@
     var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
     // configuration ===============================================================
-            //mongoose.connect(database.url);     // connect to mongoDB database on modulus.io
+            mongoose.connect(database.url);     // connect to mongoDB database on modulus.io
 
     app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
